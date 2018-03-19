e9c87bcbb2a1774e74abf8cd4d69d9bb0f081461
Mikers <mike@mike.rs>
2018-03-18 22:03:50 -0400

    Script for looping through and printing out all of the missed transactions

---
+++ missed_transactions.js
@@ -0,0 +1,25 @@
+var redis = require("redis");
+var redisClient = redis.createClient();
+
+var Web3 = require('web3')
+var web3 = new Web3();
+
+web3.setProvider(new web3.providers.HttpProvider('https://mainnet.infura.io')) //requires a full node
+
+redisClient.keys('*',function(err, keys){
+  keys.forEach(function(key,i){
+    redisClient.type(key,  function (err, type) { // get the type of the key
+        if(type=="list" && key.startsWith("balance_transfers")){ // get all of the keys that have balance transfer (blockchain transactions)
+          redisClient.lrange(key, 0, -1, function (err, transfers) {
+            transfers.forEach(function (transfer, i) {
+              transfer = JSON.parse(transfer);
+              var transaction = web3.eth.getTransaction(transfer['txHash']); // check if the transaction is actually on the blockchain
+              if(!transaction){ // transaction is not on the blockchain
+                console.log(transfer);
+              }
+            });
+          });
+        }
+    });
+  });
+});
