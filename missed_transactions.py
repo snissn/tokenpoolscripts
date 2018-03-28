8d9408c4110e03c3b0a67a629e25d4728595eca7
Mikers <mike@mike.rs>
2018-03-28 00:17:36 -0400

    confirmed key in csv

--- missed_transactions.py
+++ missed_transactions.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 import json
-fields = {'balancePaymentId': '0x9a57377db3675933a8bb12cd25651e9db80c3a11ebbd1349248a70316d22e377', 'tokenAmount': 1218624102, 'txHash': '0x4b1126ca6215eedbf60094e8a5baac5eea353e1a6c9c15bb94a25c770fa3fa0f', 'addressTo': '0x8C24092BB23394f83CDd7520FF42C437e25a3c39', 'block': 5264959}.keys()
+fields = {'balancePaymentId': '0x9a57377db3675933a8bb12cd25651e9db80c3a11ebbd1349248a70316d22e377', 'tokenAmount': 1218624102, 'txHash': '0x4b1126ca6215eedbf60094e8a5baac5eea353e1a6c9c15bb94a25c770fa3fa0f', 'addressTo': '0x8C24092BB23394f83CDd7520FF42C437e25a3c39', 'confirmed': True, 'block': 5264959}.keys()
 
 writer= csv.DictWriter(open("missed_transactions.csv",'w'), fieldnames=fields)
 writer.writeheader()
